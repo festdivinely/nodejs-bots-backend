45876a413c204d01b7073c1de19020acdfe05673
festdivinely <festusheav@gmail.com>
2025-11-11 10:03:58 -0500

    vercel8

--- src/utility/suspiciousEmail.js
+++ src/utility/suspiciousEmail.js
@@ -1,6 +1,5 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
-import { logger } from '../logger/logger.js'; // Import Pino logger
 
 // Load environment variables
 dotenv.config();
