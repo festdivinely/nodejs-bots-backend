ff6a55783d58aca12fc1c1429ea85325cc76632f
festdivinely <festusheav@gmail.com>
2025-11-12 10:31:52 -0500

    vercel30

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -23,8 +23,6 @@ async function connectDb() {
         console.log("🔗 Connecting to MongoDB...");
         cached.promise = mongoose
             .connect(process.env.MONGO_URI, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
                 serverSelectionTimeoutMS: 5000,
                 maxPoolSize: 10, // serverless-friendly
             })
